b12db0cce402f6fab235291d4c50d0749a490058
MyTruong28022004 <truongthitumy28022004@gmail.com>
2025-04-08 22:18:37 +0700

    Update GenAIServiceApplication.java

--- spring-petclinic-genai-service/src/main/java/org/springframework/samples/petclinic/genai/GenAIServiceApplication.java
+++ spring-petclinic-genai-service/src/main/java/org/springframework/samples/petclinic/genai/GenAIServiceApplication.java
@@ -27,6 +27,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class GenAIServiceApplication {
 
 	public static void main(String[] args) {
+        //LaLa
 		SpringApplication.run(GenAIServiceApplication.class, args);
 	}
 }
