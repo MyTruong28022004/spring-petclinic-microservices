c0fa0a962e4fba587632239a50b81d7f07f3037e
unknown <tranquanthuan132@gmail.com>
2025-04-09 16:10:10 +0700

    change

--- spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/VisitsServiceApplication.java
+++ spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/VisitsServiceApplication.java
@@ -25,8 +25,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class VisitsServiceApplication {
-//    hehehe
-//    test
     public static void main(String[] args) {
         SpringApplication.run(VisitsServiceApplication.class, args);
     }
