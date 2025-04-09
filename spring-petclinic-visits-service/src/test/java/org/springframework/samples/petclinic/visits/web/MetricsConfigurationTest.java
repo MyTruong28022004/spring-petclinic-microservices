3a02bcd3b1874b263ea95de5e2e43375537a72a2
unknown <tranquanthuan132@gmail.com>
2025-04-09 13:54:50 +0700

    add/test

--- spring-petclinic-visits-service/src/test/java/org/springframework/samples/petclinic/visits/web/MetricsConfigurationTest.java
+++ spring-petclinic-visits-service/src/test/java/org/springframework/samples/petclinic/visits/web/MetricsConfigurationTest.java
@@ -1,4 +1,28 @@
 package org.springframework.samples.petclinic.visits.web;
 
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.samples.petclinic.visits.config.MetricConfig;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class MetricsConfigurationTest {
+    @Test
+    public void testMetricsConfig() {
+        MetricConfig config = new MetricConfig();
+
+        MeterRegistryCustomizer<MeterRegistry> customizer = config.metricsCommonTags();
+        MeterRegistry registry = new SimpleMeterRegistry();
+        customizer.customize(registry);
+
+        registry.counter("test.counter").increment();
+        assertEquals("petclinic", registry.find("test.counter").tags("application", "petclinic").counter().getId().getTag("application"));
+
+        TimedAspect timedAspect = config.timedAspect(registry);
+        assertNotNull(timedAspect, "TimedAspect should not be null");
+    }
 }
